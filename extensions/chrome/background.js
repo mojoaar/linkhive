79a5e147f0f42248367cbc56bf223a73a644f791
Morten Johansen <mj@emsg.net>
2026-05-25 15:10:12 +0200

    Fix: set dark/light icon immediately on service worker startup

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -1,3 +1,20 @@
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.action.setIcon({ path: { 16: 'icons/icon-16.png', 48: 'icons/icon-48.png', 128: 'icons/icon-128.png' } });
-});
+function setIcon(isDark) {
+  chrome.action.setIcon({
+    path: isDark
+      ? { 16: 'icons/icon-16-dark.png', 48: 'icons/icon-48-dark.png', 128: 'icons/icon-128-dark.png' }
+      : { 16: 'icons/icon-16.png', 48: 'icons/icon-48.png', 128: 'icons/icon-128.png' }
+  });
+}
+
+function detectAndSetIcon() {
+  try {
+    var mq = self.matchMedia('(prefers-color-scheme: dark)');
+    setIcon(mq.matches);
+    mq.addEventListener('change', function (e) { setIcon(e.matches); });
+  } catch (e) {
+    setIcon(false);
+  }
+}
+
+chrome.runtime.onInstalled.addListener(detectAndSetIcon);
+detectAndSetIcon();
