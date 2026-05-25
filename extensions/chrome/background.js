6d0752340d491a6773b1b2d900624e7ee12bfc63
Morten Johansen <mj@emsg.net>
2026-05-25 15:18:08 +0200

    Fix icon switching: popup detects theme, messages background to set icon

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -1,3 +1,9 @@
 chrome.runtime.onInstalled.addListener(function () {
   console.log('LinkHive extension installed');
 });
+
+chrome.runtime.onMessage.addListener(function (request) {
+  if (request.setIcon) {
+    chrome.action.setIcon({ path: request.setIcon });
+  }
+});
